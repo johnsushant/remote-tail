ddb0de0ec8cd91c7e588c882ffd68bd6354d75b2
John Sushant Sundharam <johnsushant@outlook.com>
2017-11-05 15:51:21 +0530

    Initial Commit

---
+++ server.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import socket
+from tailf import tailf
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_address = ('0.0.0.0', 8080)
+print("Starting up on port 8080")
+sock.bind(server_address)
+sock.listen(1)
+
+while True:
+    print("Waiting for connection")
+    connection, client_address = sock.accept()
+    try:
+        input_file = connection.recv(1024)
+        print('Connection from', client_address)
+        while True:
+            for line in tailf(input_file):
+                print(line)
+                connection.sendall(line)
+    finally:
+        connection.close()

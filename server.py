fca50a16c65a0d94e9b83fdfb352be44270288d0
John Sushant Sundharam <johnsushant@outlook.com>
2017-11-05 16:46:04 +0530

    Successfully added threading to handle multiple clients

--- server.py
+++ server.py
@@ -1,26 +1,31 @@
 #!/usr/bin/env python
 
 import socket
+import thread
 from tailf import tailf
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = ('0.0.0.0', 8080)
 print("Starting up on port 8080")
 sock.bind(server_address)
-sock.listen(1)
+sock.listen(5)
+
+def socketthread(connection):
+    while True:
+        try:
+            # Recieve input file to be tailed from client
+            input_file = connection.recv(1024)
+            print('Connection from', client_address)
+            # Handle Tail Infinitely until connection is closed
+            for line in tailf(input_file):
+                print(line)
+                connection.sendall(line)
+        except socket.error:
+            connection.close()
+        finally:
+            connection.close()
 
 while True:
-    print("Waiting for connection")
+    print("Waiting for a connection")
     connection, client_address = sock.accept()
-    try:
-        # Recieve input file to be tailed from client
-        input_file = connection.recv(1024)
-        print('Connection from', client_address)
-        # Handle Tail Infinitely until connection is closed
-        for line in tailf(input_file):
-            print(line)
-            connection.sendall(line)
-    except socket.error:
-        connection.close()
-    finally:
-        connection.close()
+    thread.start_new_thread(socketthread,(connection,))
